8a324ec7e85cbbdb4084287c205ff510bc8f66ba
Unknown <103126971+HE110W0R19@users.noreply.github.com>
2022-10-31 20:47:31 +0300

    Add new work

---
+++ Registration_Blank/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void UserNsme_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (this.UserNsme.Text.Length != 0)
+                UNL.Content = "";
+            else
+                UNL.Content = "User name";
+        }
+
+        private void UserEmail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (this.UserEmail.Text.Length != 0)
+                EML.Content = "";
+            else
+                EML.Content = "Email";
+        }
+
+        private void Confirm_Email_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (this.Confirm_Email.Text.Length != 0)
+                CEL.Content = "";
+            else
+                CEL.Content = "Confirm email";
+        }
+
+        private void EnterPass(object sender, KeyEventArgs e)
+        {
+            if (this.UserPasword.Password.Length != 0)
+                PWN.Content = "";
+            else
+                PWN.Content = "Password";
+        }
+
+        private void DateSelecting(object sender, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ TextStyleRedactor/TextStyleRedactor/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextStyleRedactor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int StartIndex = 0;
+        int EndIndex = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            for (int i = 3; i <= 60; i += 3)
+            {
+                FontSizeBox.Items.Add(i.ToString());
+            }
+        }
+
+        private void ButtonBold_Click(object sender, RoutedEventArgs e)
+        {
+            this.StartIndex += 1; this.EndIndex += 2;
+            TextPointer S = AllTextBox.Document.ContentStart.GetPositionAtOffset(StartIndex, LogicalDirection.Forward);
+            TextPointer E = AllTextBox.Document.ContentStart.GetPositionAtOffset(EndIndex, LogicalDirection.Forward);
+            AllTextBox.Selection.Select(S, E);
+            TextSelection tmp = AllTextBox.Selection;
+            if (!tmp.IsEmpty)
+            {
+                tmp.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Bold);
+            }
+        }
+
+        private void ButtonItalic_Click(object sender, RoutedEventArgs e)
+        {
+            this.StartIndex += 1; this.EndIndex += 2;
+            TextPointer S = AllTextBox.Document.ContentStart.GetPositionAtOffset(StartIndex, LogicalDirection.Forward);
+            TextPointer E = AllTextBox.Document.ContentStart.GetPositionAtOffset(EndIndex, LogicalDirection.Forward);
+            AllTextBox.Selection.Select(S, E);
+            TextSelection tmp = AllTextBox.Selection;
+            if (!tmp.IsEmpty)
+            {
+                tmp.ApplyPropertyValue(TextElement.FontStyleProperty, FontStyles.Italic);
+            }
+        }
+
+        private void ButtonUnderline_Click(object sender, RoutedEventArgs e)
+        {
+            this.StartIndex += 1; this.EndIndex += 2;
+            TextPointer S = AllTextBox.Document.ContentStart.GetPositionAtOffset(StartIndex, LogicalDirection.Forward);
+            TextPointer E = AllTextBox.Document.ContentStart.GetPositionAtOffset(EndIndex, LogicalDirection.Forward);
+            AllTextBox.Selection.Select(S, E);
+            TextSelection tmp = AllTextBox.Selection;
+            if (!tmp.IsEmpty)
+            {
+                tmp.ApplyPropertyValue(TextDecoration.LocationProperty, TextDecorations.Underline);
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FontSizeBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void FontColorBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (StartText.Text.Length != 0)
+                this.StartIndex = Convert.ToInt32(StartText.Text);
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+            if (Stoptext.Text.Length != 0)
+                this.EndIndex = Convert.ToInt32(Stoptext.Text);
+        }
+    }
+}
